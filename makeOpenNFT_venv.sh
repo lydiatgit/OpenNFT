b446b3be5179b574b95a86b0710ae88664d6a711
Nina Wiedemann <ninawiedemann@Ninas-MacBook-Air.local>
2019-05-07 16:27:10 +0200

    changes more generic

--- makeOpenNFT_venv.sh
+++ makeOpenNFT_venv.sh
@@ -5,15 +5,14 @@
 ROOTPATH=$(pwd)
 VENVNAME=OpenNFT_venv
 
-python -m venv $VENVNAME
+python3.5 -m venv $VENVNAME || python3.6 -m venv $VENVNAME
 
 source $VENVNAME/bin/activate
 
 MATLAB_PATH=$(find /Applications -type d -maxdepth 1 -name "MATLAB*" | head -n 1)
 
 cd $MATLAB_PATH/extern/engines/python
-python setup.py install
+python3.5 setup.py install || python3.6 setup.py install
 
 cd $ROOTPATH
-cd $VENVNAME/bin
-pip install -r ../../requirements_mac.txt
+pip install -r requirements_mac.txt
